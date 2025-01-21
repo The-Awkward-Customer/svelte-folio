0e28526e3fccde6e3afdcf069d12939c9d25d219
PETER ABBOTT <abbottsayshi@gmail.com>
2025-01-21 15:21:08 +0100

    Initial commit: Set up project folder, VSCode workspace, and basic files

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+
+<script lang="ts">
+    let helloWorld: string = "Hello world";
+    console.log(helloWorld);
+
+</script>
+
+
+<style>   
+    .h-1 {
+        color: rgb(60, 60, 60);
+    }
+
+    .p-text{
+        color: rgb(80, 80, 80);
+    }
+</style>
+
+<main>
+<h1 class="h-1">Hello world</h1>
+<p class="p-text">First meaningful stylistic change</p>
+</main>
