bdba31034873b69a34c285165db2db75c87a0103
PETER ABBOTT <abbottsayshi@gmail.com>
2025-01-22 19:39:42 +0100

    fixed db connection like a boss

--- frontend/src/lib/server/db/index.ts
+++ frontend/src/lib/server/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = postgres(env.DATABASE_URL);
+if (!env.SUPABASE_URL) throw new Error('SUPABASE_URL is not set');
+const client = postgres(env.SUPABASE_URL);
 export const db = drizzle(client);
