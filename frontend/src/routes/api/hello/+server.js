27ac75abc16dd4986a88af9d91ed4fe9f0dbad55
PETER ABBOTT <abbottsayshi@gmail.com>
2025-01-22 19:12:29 +0100

    Integrated API endpoint into SvelteKit: /api/hello

---
+++ frontend/src/routes/api/hello/+server.js
@@ -0,0 +1,7 @@
+// +server.js
+export function GET() {
+	const data = { message: 'Hello World from SvelteKit endpoint' };
+	return new Response(JSON.stringify(data), {
+		headers: { 'Content-Type': 'application/json' }
+	});
+}
