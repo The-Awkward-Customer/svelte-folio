cf27b05353e17c0527b6e946a07770e66644a660
The-Awkward-Customer <abbottsayshi@gmail.com>
2025-04-04 08:17:05 +0100

    removed failing imports

--- frontend/src/routes/caseStudies/+page.svelte
+++ frontend/src/routes/caseStudies/+page.svelte
@@ -6,9 +6,7 @@
     import BasicLayout from "$lib/components/layout/BasicLayout.svelte";
     import TopBar from "$lib/components/navigation/TopBar.svelte";
     import LinkList from "$lib/components/actions/LinkList.svelte";
-    import HeroHeader from "$lib/components/content/HeroHeader.svelte";
     import SideBar from "$lib/components/navigation/SideBar.svelte";
-    import SimpleContentSection from "$lib/components/content/SimpleContentSection.svelte";
     import VideoTile from "$lib/components/media/VideoTile.svelte";
     $: currentPath = $page.url.pathname;
 </script>
@@ -27,7 +25,6 @@
           { path: '/visualDesign', label: 'visual design' },
         ]}
         vertical={true}
-        showBorder={true}
         ariaLabel="Main Navigation"
      />
 
@@ -36,7 +33,7 @@
 
  <div slot="main">
 
-<HeroHeader/>
+<p>Case Studies</p>
 
 
 <VideoTile 
