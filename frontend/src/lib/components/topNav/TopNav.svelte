c64578440849e3fb1bf98a6ff692d51763c08b86
The-Awkward-Customer <abbottsayshi@gmail.com>
2025-05-28 11:58:08 +0200

    refactor: added content and link-list to top nav as made it responsive

--- frontend/src/lib/components/topNav/TopNav.svelte
+++ frontend/src/lib/components/topNav/TopNav.svelte
@@ -3,29 +3,72 @@
 import type { Snippet } from 'svelte';
 
 
+import LinkList from '../navigation/LinkList.svelte';
+
+  interface LinkItem {
+    label: string;
+    href: string;
+  }
+
+  interface LinkListArray {
+    list?: LinkItem[];
+  }
+
+  let listData: LinkItem[] = [ 
+    { label: "Index", href: "/" },
+    { label: "Graphics", href: "/graphics" }
+  ];
+
+
 interface TopNavProps{
     children? : Snippet
 }
 
-let { children }: TopNavProps = $props();
+
+let { list = listData, children }: LinkListArray & TopNavProps = $props();
 
 
 </script>
 
 <nav>
-    {@render children?.()}
+    <div>
+        <p>Peter Abbott</p>
+        <p>A Developer, Designer & Mentor based in Madrid, Spain.</p>
+    </div>
+    <LinkList {list} />
 </nav>
 
 <style>
 nav{
     display: inline-flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    align-items: center;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
     width: 100%;
     max-width: 1024px;
     padding-top: 12px;
     padding-bottom: 12px;
+    gap: 1em;
+}
+
+div{
+    display: flex;
+    flex-direction: column;
+    color: rgb(var(--color-fg-primary));
+    font-size: var(--fs-300);
+    font-weight: var(--fw-semibold);
+    max-width: 32ch;
+}
+
+div p:nth-child(2){
+ font-weight: var(--fw-regular);
+ color:rgb(var(--color-fg-secondary))
+}
+
+@media (min-width: 896px){
+    nav{
+        flex-direction: row;
+    }
 }
 
 </style>
