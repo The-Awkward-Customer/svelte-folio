4c4c01401955f2888270dfe25d801aee15ba279f
The-Awkward-Customer <abbottsayshi@gmail.com>
2025-05-28 11:21:35 +0200

    added accordian content

---
+++ frontend/src/lib/components/accordian/accordionItems/Experience.svelte
@@ -0,0 +1,32 @@
+<script lang='ts'>
+
+    let P1 : string = "Most Recently I was the Cofound designer at GoDesk, before which I led the Design System team at Fresha."
+    let P2 : string = "I also have work as contracting and with agencies."
+    let P3 : string = "Currently Available for work in Q3-2025"
+
+</script>
+
+
+<div>
+    <p>{P1}</p>
+    <p>{P2}</p>
+    <p class="p-3">{P3}</p>
+</div>
+
+
+<style>
+
+    div{
+        display: inline-flex;
+        flex-direction: column;
+        font-size: clamp(18px, 4vw, 28px);
+        gap: 0.5em;
+        padding-left: 2em;
+    }
+
+    .p-3{
+        color:green;
+        font-weight: var(--fw-regular);
+    }
+
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,16 +5,10 @@
 
     // components
     import { page } from "$app/stores";
-    import BasicLayout from "$lib/components/layout/BasicLayout.svelte";
-    import TopBar from "$lib/components/navigation/TopBar.svelte";
-    import LinkList from "$lib/components/actions/LinkList.svelte";
-    import SideBar from "$lib/components/navigation/SideBar.svelte";
-    import { HeroRoot, HeroTitle, HeroIntro, HeroButton } from "$lib/components/content/Hero";
-    import Callout from "$lib/components/feedback/Callout.svelte";
     import { dialogManager } from '$lib/stores/dialogManager';
-    import DialogRoot from '$lib/components/Dialog/DialogRoot.svelte';
-    import IntroText from "$lib/components/content/IntroText.svelte";
     import Accordian from "$lib/components/accordian/Accordian.svelte";
+    import { Introduction, Experience, Process} from '$lib/components/accordian/accordionItems'
+	import IntroText from "$lib/components/content/IntroText.svelte";
 
     //Page Content
     let HeroIntroText: string = "Currently based in sunny Madrid, and available for new projects in Q3 2025.";
@@ -36,15 +30,15 @@
 </script>
 <ol>
 <Accordian number="1"  label="Start here" >
-    <IntroText/>
+    <Introduction/>
 </Accordian>
 
 <Accordian number="2"  label="Experience">
-    <IntroText/>
+    <Experience/>
 </Accordian>
 
 <Accordian number="3"  label="How I work">
-    <IntroText/>
+     <Process/>
 </Accordian>
 </ol>
 
