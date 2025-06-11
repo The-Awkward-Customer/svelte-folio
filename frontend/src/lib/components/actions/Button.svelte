4b3945197d59f893b3eec55369da44b6574a275f
The-Awkward-Customer <abbottsayshi@gmail.com>
2025-06-12 00:10:19 +0200

    add: modal controls to graphics page. minor changes to card elements

--- frontend/src/lib/components/actions/Button.svelte
+++ frontend/src/lib/components/actions/Button.svelte
@@ -1,75 +1,62 @@
 <script lang="ts">
-	import type { handle } from "../../../hooks.server";
+	import type { handle } from '../../../hooks.server';
 
-    // Props interface
+	// Props interface
 
-    type buttonVariants = 'ghost' | 'strong';
+	type buttonVariants = 'ghost' | 'strong';
 
+	type buttonRole = 'button' | 'submit' | 'reset';
 
+	interface ButtonProps {
+		variant?: buttonVariants;
+		type?: buttonRole;
+		disabled?: boolean;
+		// ariaLabel?: string; // Combine with label
+		label: string;
+		handleClick: () => void;
+	}
 
-    type buttonRole = 'button' | 'submit' | 'reset';
+	let { label = 'Replace me', variant = 'ghost', handleClick }: ButtonProps = $props();
+</script>
 
+<span class="btn-root">
+	<button class={` btn ${variant} `} aria-label={label} onclick={handleClick}>
+		<span class="btn-layout">
+			<span class="middle">{label}</span>
+		</span>
+	</button>
+</span>
 
-    interface ButtonProps {
-        variant?: buttonVariants;
-        type?: buttonRole;
-        disabled?: boolean;
-        // ariaLabel?: string; // Combine with label
-        label: string
-        handleClick:  () => void
-    }
+<style>
+	.btn {
+		border: none;
+		border-radius: var(--bdr-radius-small);
+		font-weight: var(--fw-semibold);
+		height: 44px;
+		padding-left: 8px;
+		padding-right: 8px;
+	}
 
-    let { label="Replace me", variant="ghost", handleClick} : ButtonProps = $props();
+	.ghost {
+		background-color: rgb(255, 255, 255);
+	}
 
-   
+	.ghost:hover {
+		background-color: rgb(218, 218, 225);
+	}
 
-  </script>
-  
-  <span class="btn-root" >
-  <button class={` btn ${variant} `} aria-label={label}  onclick={handleClick}>
-    <span class="btn-layout">
+	.ghost:active {
+		background-color: rgb(184, 185, 187);
+	}
 
-      <span class="middle">{label}</span>
-
-    </span>
-  </button>
-  </span>
-  
-  <style>
-    .btn{
-      border: none;
-      border-radius: var(--bdr-radius-pill);
-      font-weight: var(--fw-semibold);
-      height: 44px;
-      padding-left: 8px;
-      padding-right: 8px;
-    }
-
-    .ghost{
-      background-color: rgb(255, 255, 255);
-    }
-
-    .ghost:hover{
-       background-color: rgb(218, 218, 225);
-    }
-
-    .ghost:active{
-        background-color: rgb(184, 185, 187);
-
-    }
-    
-
-    .btn-layout{
-      display: inline-grid;
-      grid-template-columns: auto;
-      grid-template-rows: 1fr;
-      gap: 4px;
-      padding-left: 12px;
-      padding-top: 8px;
-      padding-right: 12px;
-      padding-bottom: 8px;
-    }
-
-    
-    
-  </style>
+	.btn-layout {
+		display: inline-grid;
+		grid-template-columns: auto;
+		grid-template-rows: 1fr;
+		gap: 4px;
+		padding-left: 12px;
+		padding-top: 8px;
+		padding-right: 12px;
+		padding-bottom: 8px;
+	}
+</style>
--- frontend/src/lib/components/primatives/Tag.svelte
+++ frontend/src/lib/components/primatives/Tag.svelte
@@ -1,27 +1,43 @@
 <script lang="ts">
+	type TagColor = 'currentColor' | string;
+
 	interface Props {
 		label: string;
+		beforeColor?: TagColor;
 	}
 
-	let { label }: Props = $props();
+	let { label, beforeColor = 'currentColor' }: Props = $props();
 </script>
 
-<span class="tag">
+<span class="tag" style="--before-color: {beforeColor}">
 	{label}
 </span>
 
 <style>
 	.tag {
-		display: inline-block;
-		padding: 4px 8px;
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		padding-right: 2px;
+		padding-top: 3px;
+		padding-bottom: 4px;
 		font-size: var(--fs-250);
 		font-weight: var(--fw-semibold);
-		text-transform: capitalize;
-		border-radius: var(--bdr-radius-tiny);
-		background-color: rgba(0, 0, 0, 1);
-		border: 1px solid black;
-		color: rgb(var(--fg-text-primary));
-		border: 1px solid rgba(59, 130, 246, 0.2);
+		text-transform: uppercase;
+		background-color: rgba(0, 0, 0, 0);
+		color: rgb(var(--fg-text-inverse));
+		border-bottom: 1px solid rgba(0, 0, 0, 0.5);
 		white-space: nowrap;
+	}
+
+	.tag::before {
+		content: '';
+		display: inline-block;
+		width: 0.6em;
+		height: 0.6em;
+		background: var(--before-color, currentColor);
+		border-radius: 2px;
+		margin-right: 3px;
+		vertical-align: middle;
 	}
 </style>
